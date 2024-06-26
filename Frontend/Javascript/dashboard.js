fd5d6224537e8fad0b353542f319337db6570c27
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2024-06-26 18:40:01 +0400

    new file:   Frontend/Javascript/dashboard.js 	modified:   Frontend/html/Reset-Password.html 	modified:   Frontend/html/dashboard.html

---
+++ Frontend/Javascript/dashboard.js
@@ -0,0 +1,16 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+
+    },
+    methods: {
+        toggleSidebar() {
+            let sidebar = document.querySelector(".sidebar");
+            sidebar.classList.toggle("active");
+        }
+},
+mounted() {
+    let btn = document.getElementById('hamBtn');
+    btn.addEventListener('click', this.toggleSidebar);
+}
+})
